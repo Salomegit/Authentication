f2dadb2967223c119a2c8b48d770bff416b5b0c5
Salomegit <sallygithinji72@gmail.com>
2024-07-17 17:03:04 +0300

    breeze library added for authentication

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,31 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LandingController;
-use App\Http\Controllers\WelcomeController;
-use App\Http\Controllers\NoteController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-// Route::get('/', [LandingController::class,"landing"] ) -> name("landing");
-// Route::get('/welcome', [WelcomeController::class,"welcome"] ) -> name("welcome");
-// Route::get("/note",[NoteController::class,"index"]) -> name("note.index");
-// Route::get("/note/create",[NoteController::class,"create"]) -> name("note.create");
-// Route::post("/note",[NoteController::class,"store"]) -> name("note.store");
-// Route::get("/note/{id}",[NoteController::class,"show"]) -> name("note.show");
-// Route::get("/note/{id}/edit",[NoteController::class,"edit"]) -> name("note.edit");
-// Route::put("/note/{id}",[NoteController::class,"update"]) -> name("note.update");
-// Route::delete("/note/{id}",[NoteController::class,"destroy"]) -> name("note.destroy");
+Route::get('/', function () {
+    return view('welcome');
+});
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route :: resource("note",NoteController :: class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
