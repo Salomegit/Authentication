ca2ee4db8cfbc22363da3abeb4c7e317cd6aa432
Salomegit <sallygithinji72@gmail.com>
2024-07-22 17:07:56 +0300

    env updated

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\NoteController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,9 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::middleware(['auth','verified'])->group(
+    function(){
+    // Route::get('/', [LandingController::class,"landing"] ) -> name("landing");
+// Route::get('/welcome', [WelcomeController::class,"welcome"] ) -> name("welcome");
+// Route::get("/note",[NoteController::class,"index"]) -> name("note.index");
+// Route::get("/note/create",[NoteController::class,"create"]) -> name("note.create");
+// Route::post("/note",[NoteController::class,"store"]) -> name("note.store");
+// Route::get("/note/{id}",[NoteController::class,"show"]) -> name("note.show");
+// Route::get("/note/{id}/edit",[NoteController::class,"edit"]) -> name("note.edit");
+// Route::put("/note/{id}",[NoteController::class,"update"]) -> name("note.update");
+// Route::delete("/note/{id}",[NoteController::class,"destroy"]) -> name("note.destroy");
+
+Route :: resource("note",NoteController :: class);
+    }
+);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
